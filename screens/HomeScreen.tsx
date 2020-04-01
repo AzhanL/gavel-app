1e17896f1e1564cbde360ae1cdeb36fbe31631a1
azhan-latif <azhanx@protonmail.com>
2020-03-31 21:30:18 -0400

    Fix centering "No bookmarks yet".

--- screens/HomeScreen.tsx
+++ screens/HomeScreen.tsx
@@ -8,7 +8,8 @@ import {
   Card,
   Button,
   Badge,
-  Colors
+  Colors,
+  Paragraph
 } from "react-native-paper";
 import { useLazyQuery, useQuery } from "@apollo/react-hooks";
 import {
@@ -47,7 +48,7 @@ export default function HomeScreen({ navigation }) {
     // Don't cache anything
     fetchPolicy: "no-cache",
     // Poll every 1 second
-    pollInterval: 1000
+    pollInterval: 500
   });
 
   // useEffect Hook
@@ -66,72 +67,69 @@ export default function HomeScreen({ navigation }) {
     <Container>
       {/* App bar  */}
       <Appbar.Header style={{ height: 70 }}>
-        <Appbar.Action icon="home"/>
+        <Appbar.Action icon="home" />
         <Appbar.Content title="HOME" />
         <Appbar.Action icon="dots-vertical" />
       </Appbar.Header>
 
-
       {/* Loading circle at start */}
-      {subscriptions_loading ? (
+      {subscriptions_loading && (
         <View
           style={{ flex: 1, justifyContent: "center", alignItems: "center" }}
         >
           <ActivityIndicator animating={true} color="#20272F" size="large" />
         </View>
-      ) : (
-        <></>
       )}
 
       {/* Display content when done loading*/}
-      {!subscriptions_loading ? (
-        <Content padder>
-           <List.Accordion  title={`SUBSCRIBED CASES (${sortedSubscriptions.length})`} left={props => <List.Icon {...props} icon="bookmark" />}>
+      {!subscriptions_loading && subscriptions.getUnread.length >= 1 && (
+        <Content>
           {sortedSubscriptions.map((subscription, i) => (
-           
-              <List.Item
-                key={i}
-                title={subscription.courtFileNumber}
-                style={{ backgroundColor: Colors.grey300 }}
-                // Set the description to "n new hearings"
-                description={
-                  subscription.itemCount > 0
-                    ? subscription.itemCount.toString() +
-                      " new " +
-                      // Plural/Singular
-                      (subscription.itemCount >= 1 ? "hearings" : "hearing")
-                    : ""
-                }
-                left={props => <List.Icon {...props} icon="folder-account" />}
-                right={props => <List.Icon {...props} icon="arrow-right" />}
-                onPress={() => {
-                  // Open the navigation
-                  navigation.navigate("Modals", {
-                    screen: "HearingDetail",
-                    params: {
-                      courtFileNumber: subscription.courtFileNumber
+            <List.Item
+              key={i}
+              title={subscription.courtFileNumber}
+              style={{ backgroundColor: Colors.grey300 }}
+              // Set the description to "n new hearings"
+              description={
+                subscription.itemCount > 0
+                  ? subscription.itemCount.toString() +
+                    " new " +
+                    // Plural/Singular
+                    (subscription.itemCount >= 1 ? "hearings" : "hearing")
+                  : ""
+              }
+              left={props => <List.Icon {...props} icon="folder-account" />}
+              right={props => <List.Icon {...props} icon="arrow-right" />}
+              onPress={() => {
+                // Open the navigation
+                navigation.navigate("Modals", {
+                  screen: "HearingDetail",
+                  params: {
+                    courtFileNumber: subscription.courtFileNumber
+                  }
+                });
+              }}
+              // Bold the title if there are any unread hearings
+              titleStyle={
+                subscription.itemCount > 0
+                  ? {
+                      fontWeight: "bold"
                     }
-                  });
-                }}
-                // Bold the title if there are any unread hearings
-                titleStyle={
-                  subscription.itemCount > 0
-                    ? {
-                        fontWeight: "bold"
-                      }
-                    : {}
-                }
-              />
-             
+                  : {}
+              }
+            />
           ))}
-          </List.Accordion>
         </Content>
-      ) : (
-        <>
-          <View style={{ flex: 1, alignItems: "center" }}>
-            <Text style={{marginTop:200, fontWeight:'bold'}}>No bookmarks yet</Text>
-          </View>
-        </>
+      )}
+
+      {/* No bookmarks */}
+      {!subscriptions_loading && subscriptions.getUnread.length <= 0 &&  (
+        <View
+          style={{ flex: 1, justifyContent: "center", alignItems: "center" }}
+        >
+          <Paragraph>No bookmarks yet</Paragraph>
+          <List.Icon icon="bookmark" />
+        </View>
       )}
     </Container>
   );
